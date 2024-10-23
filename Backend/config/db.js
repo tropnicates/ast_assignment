aa4f7f9cff1b0192e3ae2239c467a54410c06104
Kundan Kumar <kun9577@gmail.com>
2024-10-23 17:34:54 +0530

    final submission

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        // await mongoose.connect("mongodb://127.0.0.1:27017/ruleEngine");
-        await mongoose.connect('mongodb+srv://kiran18202:6zoA6gNT2Un7VwZj@ruleengine.rvs8z.mongodb.net/ruleEngine');
+        await mongoose.connect('mongodb+srv://tropnicates:BQZ7PIxJHKdroqex@fullstack-blog-project.otgvrqz.mongodb.net/?retryWrites=true&w=majority');
         console.log("MongoDB Connected");
     } catch (err) {
         console.error("MongoDB Connection Error", err);
